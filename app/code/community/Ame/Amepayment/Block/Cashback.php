c320bc63041e2a93cfbdeb4f2e3207f466193abf
Gustavo Ulyssea <gustavo.ulyssea@gmail.com>
2021-02-05 18:14:35 -0300

    Cashback percent on product page issue solution

--- app/code/community/Ame/Amepayment/Block/Cashback.php
+++ app/code/community/Ame/Amepayment/Block/Cashback.php
@@ -33,13 +33,7 @@ class Ame_Amepayment_Block_Cashback extends Mage_Core_Block_Template
         $value = $product->getFinalPrice() * $this->getCashbackPercent() * 0.01;
         return $value;
     }
-
     public function getCashbackPercent(){
-        $storeid = Mage::app()->getStore()->getStoreId();
-        $percent = Mage::getStoreConfig('ame/general/cashback_value', $storeid);
-        return $percent;
+        return $percent = Mage::helper('amepayment/api')->getCashBackPercent();
     }
-
-
-
 }
